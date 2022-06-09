02d97d6f9c40b3f76e08c85c8316418933743bda
neptune0170 <yg172014@gmail.com>
2022-06-09 21:03:52 +0530

    longest common subsequence plain reccursion

--- dp.cpp
+++ dp.cpp
@@ -1,29 +1,39 @@
 #include<iostream>
-#include <string.h>
+#include<string>
 using namespace std;
-
-int memo[1000000];
-int fib(int n)
+// int memo[1000][1000];
+int lcs(string & s1,string& s2,int n,int m)
 {
-  if(memo[n]==-1)
+  if(m==0 || n==0)
   {
-    int res;
-    if(n==0 || n==1)
-    {
-      return n;
-    }
-    else{
-      res=fib(n-1)+fib(n-2);
-    }
-    memo[n]=res;
+    return 0;
   }
-  return memo[n];
+  // if last element are same
+  if(s1[n-1]==s2[m-1])
+  {
+    return 1+lcs(s1,s2,n-1,m-1);
+  }
+  //if last element are not equal
+  else{
+    //taking maximum 
+    return  max(
+     //remove end element from 1st string 
+    lcs(s1,s2,n,m-1),
+     //remove end element from 2nd string
+    lcs(s1,s2,n-1,m));
+
+  } 
+
 }
 
 int main()
 {
-  int n=5;
-  memset(memo,-1,sizeof(memo));
-  cout<<fib(6);
- 
-  }
+string s1="AXYZ", s2="BAZ";
+	
+	int n = 4, m = 3;
+	
+	// memset(memo,-1,sizeof(memo));
+	
+ int ans= lcs(s1,s2,n,m);
+ cout<<ans;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,39 @@
-   if(arr[start]>arr[end]){
+#include<iostream>
+#include<string>
+using namespace std;
+// int memo[1000][1000];
+int lcs(string & s1,string& s2,int n,int m)
+{
+  if(m==0 || n==0)
+  {
+    return n;
+  }
+  // if last element are same
+  if(s1[m-1]==s2[n-1])
+  {
+    return 1+lcs(s1,s2,m-1,n-1);
+  }
+  //if last element are not equal
+  else{
+    //taking maximum 
+    return  max(
+     //remove end element from 1st string 
+    lcs(s1,s2,m-1,n),
+     //remove end element from 2nd string
+    lcs(s1,s2,m,n-1));
 
+  } 
 
-       }
+}
+
+int main()
+{
+string s1="AXYZ", s2="BAZ";
+	
+	int n = 4, m = 3;
+	
+	// memset(memo,-1,sizeof(memo));
+	
+ int ans= lcs(s1,s2,n,m);
+ cout<<ans;
+}
